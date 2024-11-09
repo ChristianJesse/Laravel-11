6cbd19d57690f37e2a1e70497585e7eb3bfcb70c
christianjesse.oreto <christianjesse.oreto@ionics-ems.com>
2024-11-09 18:17:38 +0800

    feat

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,4 +3,10 @@
         Home Page
     </x-slot:heading>
     <h1>This is Home page</h1>
+
+    @foreach ($jobs as $job )
+        <li>{{ $job['title'] }}: Pays {{ $job['salary'] }}</li>
+    @endforeach
+
+
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,8 +9,55 @@ Route::get('/', function () {
 });
 
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs',[
+        'jobs' => [
+            [
+                'id' => '1',
+                'title' => 'Director',
+                'salary' => '$50,00',
+            ],
+
+            [
+                'id' => '2',
+                'title' => 'Programmer',
+                'salary' => '$10,00',
+            ],
+
+            [
+                'id' => '3',
+                'title' => 'Teacher',
+                'salary' => '$40,00',
+            ],
+        ]
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+        $jobs = [
+            [
+                'id' => '1',
+                'title' => 'Director',
+                'salary' => '$50,00',
+            ],
+
+            [
+                'id' => '2',
+                'title' => 'Programmer',
+                'salary' => '$10,00',
+            ],
+
+            [
+                'id' => '3',
+                'title' => 'Teacher',
+                'salary' => '$40,00',
+            ],
+        ];
+
+        $job = Arr::first($jobs, fn($job) => $job['id'] = $id);
+        dd($job);
+    // return view('job',['job' => $job]);
+
 });
 
 
